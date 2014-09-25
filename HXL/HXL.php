9951218d3343862174243a78546ce675811f634a
David Megginson <david.megginson@megginson.com>
2014-09-25 16:29:22 -0400

    Parsing compact disaggregated OK, but not handling afterwards.

--- HXL/HXL.php
+++ HXL/HXL.php
@@ -6,6 +6,7 @@
  */
 
 require_once(__DIR__ . '/HXLColumn.php');
+require_once(__DIR__ . '/HXLColSpec.php');
 require_once(__DIR__ . '/HXLRow.php');
 require_once(__DIR__ . '/HXLValue.php');
 require_once(__DIR__ . '/HXLReader.php');
---
+++ HXL/HXLColSpec.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * A column specification in a HXL file.
+ *
+ * Might be using compact disaggregated notation to represent more
+ * than one logical HXL column.
+ *
+ * Started by David Megginson, September 2014.
+ */
+class HXLColSpec {
+
+  public $column;
+
+  public $fixedColumn;
+
+  public $fixedValue;
+
+  public function __construct($column, $fixedColumn = null, $fixedValue = null) {
+    $this->column = $column;
+    $this->fixedColumn = $fixedColumn;
+    $this->fixedValue = $fixedValue;
+  }
+
+}
