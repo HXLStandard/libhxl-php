0c4bb2856eb5cfdcdf4c344bf4cf647b5b972af0
David Megginson <david.megginson@megginson.com>
2014-09-26 13:23:18 -0400

    New class to hold logic for a table specification.

--- HXL/HXL.php
+++ HXL/HXL.php
@@ -5,8 +5,9 @@
  * May put an actual class here later (e.g. for PEAR).
  */
 
-require_once(__DIR__ . '/HXLColumn.php');
+require_once(__DIR__ . '/HXLTableSpec.php');
 require_once(__DIR__ . '/HXLColSpec.php');
+require_once(__DIR__ . '/HXLColumn.php');
 require_once(__DIR__ . '/HXLRow.php');
 require_once(__DIR__ . '/HXLValue.php');
 require_once(__DIR__ . '/HXLReader.php');
